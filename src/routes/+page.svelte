7010d98a1eb5885733047e89ae3c94249c227ddd
Koshelev Kirill <olegartor7@gmail.com>
2023-02-22 16:18:36 +0300

    init sveltekit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,4 @@
+<div class="h-screen w-screen grid place-content-center">
+	<h1>Welcome to SvelteKit</h1>
+	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</div>
